8f96009fc746fac2d13f888d174944a4757a6148
MuhamadRifai1311 <149478908+MuhamadRifai1311@users.noreply.github.com>
2026-02-12 11:23:27 +0700

    responsive mobile

--- app/pages/layanan.vue
+++ app/pages/layanan.vue
@@ -21,12 +21,12 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="px-35 py-25">
+  <div class="px-5 lg:px-35 pt-25 lg:pt-0 lg:py-25">
     <div
-      class="flex flex-col justify-center items-center w-full h-96 gap-3 mb-30"
+      class="flex flex-col justify-center items-center w-full lg:h-96 gap-3 mb-15 lg:mb-30"
     >
-      <h2 class="text-black font-bold text-[72px]">Rate & Services</h2>
-      <p class="text-center text-[17px] leading-10 px-30">
+      <h2 class="text-black font-bold text-[36px] lg:text-[72px]">Rate & Services</h2>
+      <p class="text-center text-[13px] lg:text-[17px] leading-10 lg:px-30">
         Di Circle Studi, setiap proyek berangkat dari kejelasan tujuan,
         identitas, dan arah brand. Kami membantu brand membangun fondasi visual
         dan komunikasi yang konsisten, relevan, dan berkelanjutan. Setiap
@@ -35,7 +35,7 @@ onMounted(() => {
       </p>
     </div>
     <div
-      class="flex items-center justify-between border-b border-t py-8 border-gray-200 mb-30"
+      class="flex flex-col lg:flex-row items-center justify-center lg:justify-between border-b-0 lg:border-b  border-t-0 lg:border-t py-8 border-gray-200 mb-15 lg:mb-30 gap-5 lg:gap-0"
     >
       <div
         class=""
@@ -46,7 +46,7 @@ onMounted(() => {
         ]"
         :key="value"
       >
-        <h2 class="text-3xl font-bold">{{ value }}</h2>
+        <h2 class="text-2xl lg:text-3xl font-bold">{{ value }}</h2>
       </div>
     </div>
     <div
@@ -54,23 +54,23 @@ onMounted(() => {
       v-for="value in services"
       :key="value.desc"
     >
-      <div class="py-20 border-b border-gray-200">
-        <h2 class="text-[52px]">{{ value.title }}</h2>
+      <div class="py-10 lg:py-20 border-b border-gray-200">
+        <h2 class=" text-[26px] lg:text-[52px]">{{ value.title }}</h2>
       </div>
-      <div class="grid grid-cols-3">
-        <div class="flex flex-col gap-3 col-span-2">
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-0">
+        <div class="flex flex-col gap-3 col-span-2 w-full">
           <div class="flex items-center gap-2">
             <div class="bg-red-500 rounded-full h-3 w-3"></div>
             <h2 class="text-[20px] font-bold">{{ value.title2 }}</h2>
           </div>
-          <p class="text-black/50 w-3/4">{{ value.desc }}</p>
+          <p class="text-black/50 w-full lg:w-3/4">{{ value.desc }}</p>
         </div>
         <div class="flex flex-col gap-2">
           <h2 class="font-bold text-black">IDR.{{ value.price }}</h2>
           <button class="text-center border w-fit text-[12px] px-1">
             {{ value.button }}
           </button>
-          <p class="text-black/50 text-xs w-3/4">{{ value.desc2 }}</p>
+          <p class="text-black/50 text-xs w-full lg:w-3/4">{{ value.desc2 }}</p>
         </div>
       </div>
     </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
+  modules: ['shadcn-nuxt'],
 })
